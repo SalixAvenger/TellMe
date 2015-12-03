59c313b4d9aae424b9e250db2bddd496fb805e92
Joel <joel@joeledlund.se>
2015-12-03 12:28:22 +0100

    Added modes

--- app/js/controller/index.js
+++ app/js/controller/index.js
@@ -4,6 +4,8 @@ module.exports = angular.module('TellME.controllers', [])
     .controller('HomeCtrl', require('./home.controller'))
     .controller('DevicesCtrl', require('./listDevices.controller'))
     .controller('WeatherCtrl', require('./weather.controller'))
+    .controller('ModesCtrl', require('./modes.controller'))
 
     .directive('listDevicesDirective', require('./listDevices.directive'))
+    .directive('modesDirective', require('./modes.directive'))
     .directive('weatherDirective', require('./weather.directive'));
